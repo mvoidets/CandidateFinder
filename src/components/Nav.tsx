51addfa20f8add0b7fc85d3c61e358035329a867
mvoidets <mvoidets@yahoo.com>
2024-11-12 14:34:01 -0700

    key was expired

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,7 +3,7 @@ import { Link, useLocation } from 'react-router-dom';
 
 
 const Nav = () => {
-  // TODO: Add necessary code to display the navigation bar and link between the pages
+  
   const currentPage = useLocation().pathname;
 
   return (
--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,4 +1,4 @@
-// TODO: Create an interface for the Candidate objects returned by the API
+
 interface Candidate {
 readonly id: number;
 readonly name: string;
