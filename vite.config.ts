52cd638043214f886cf22506a6364798536ea810
mvoidets <mvoidets@yahoo.com>
2024-11-12 21:54:20 -0700

    added port

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ export default defineConfig({
   plugins: [react()],
   server: {
 
-    port: process.env.PORT || 3000 // Use the PORT environment variable if available, otherwise default to 3000
-
+    port: process.env.PORT || 3000 
   }
 });
